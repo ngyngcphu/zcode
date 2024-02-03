a622352e9a85a4e5a017793b122199d13efdd236
ngyngcphu <nguyenngocphu225@gmail.com>
2024-02-03 17:36:36 +0700

    fix(expression): change logic of rule to satisfy non-associative property

--- src/main/zcode/parser/ZCode.g4
+++ src/main/zcode/parser/ZCode.g4
@@ -13,10 +13,10 @@ options {
 program: NEWLINE* ((varDecl NEWLINE+ | functionDecl))+ EOF;
 
 varDecl: (NUMBER | BOOL | STRING | DYNAMIC) IDENTIFIER (
-		ASSIGN expr
+		ASSIGN expression
 	)?
-	| VAR IDENTIFIER ASSIGN expr
-	| array_type (ASSIGN expr)?;
+	| VAR IDENTIFIER ASSIGN expression
+	| array_type (ASSIGN expression)?;
 
 functionDecl:
 	FUNC IDENTIFIER LB paramList RB NEWLINE* (
@@ -44,22 +44,22 @@ statement_list:
 	statement_type NEWLINE+ statement_list
 	| statement_type NEWLINE*;
 
-assign_stat: lhs ASSIGN expr;
+assign_stat: lhs ASSIGN expression;
 lhs: IDENTIFIER | index_expr;
 
 if_stat: if_fragment elif_fragment*? else_fragment?;
-if_fragment: IF LB expr RB NEWLINE* statement_list;
-elif_fragment: ELIF LB expr RB NEWLINE* statement_list;
+if_fragment: IF LB expression RB NEWLINE* statement_list;
+elif_fragment: ELIF LB expression RB NEWLINE* statement_list;
 else_fragment: ELSE statement_list;
 
 for_stat:
-	FOR IDENTIFIER UNTIL expr BY expr NEWLINE* statement_list;
+	FOR IDENTIFIER UNTIL expression BY expression NEWLINE* statement_list;
 
-return_stat: RETURN expr?;
+return_stat: RETURN expression?;
 
 func_call_stat: (IDENTIFIER LB arglist RB) | io_function;
 arglist: argprime |;
-argprime: expr COMMA argprime | expr;
+argprime: expression COMMA argprime | expression;
 
 block_stat: BEGIN NEWLINE+ statement_list? END;
 
@@ -72,11 +72,11 @@ io_function:
 	| write_string;
 
 read_number: 'readNumber' LB RB;
-write_number: 'writeNumber' LB expr RB;
+write_number: 'writeNumber' LB expression RB;
 read_bool: 'readBool' LB RB;
-write_bool: 'writeBool' LB expr RB;
+write_bool: 'writeBool' LB expression RB;
 read_string: 'readString' LB RB;
-write_string: 'writeString' LB expr RB;
+write_string: 'writeString' LB expression RB;
 
 array_type: (NUMBER | STRING | BOOL) IDENTIFIER LS dimension_list RS;
 dimension_list: NUMBERLIT COMMA dimension_list | NUMBERLIT;
@@ -87,18 +87,12 @@ array_elements:
 	| array_nested_or_expression;
 array_nested_or_expression: array_value | exprlist;
 
-exprlist: expr COMMA exprlist | expr;
+exprlist: expression COMMA exprlist | expression;
 index_expr: (IDENTIFIER | func_call_stat) LS exprlist RS;
 
-expr:
-	LB expr RB
-	| index_expr
-	| <assoc = right> SUB_OF expr
-	| <assoc = right> NOT_OP expr
-	| expr (MUL_OF | DIV_OF | MOD_OF) expr
-	| expr (ADD_OF | SUB_OF) expr
-	| expr (AND_OP | OR_OP) expr
-	| expr (
+expression: expression_1 CONCAT_OP expression_1 | expression_1;
+expression_1:
+	expression_2 (
 		EQUAL_OP
 		| STRING_COMPARE_OP
 		| NOT_EQUAL_OP
@@ -106,10 +100,24 @@ expr:
 		| GREATER_THAN_OP
 		| LESS_EQUAL_OP
 		| GREATER_EQUAL_OP
-	) expr
-	| expr CONCAT_OP expr
-	| array_value
+	) expression_2
+	| expression_2;
+expression_2:
+	expression_2 (AND_OP | OR_OP) expression_3
+	| expression_3;
+expression_3:
+	expression_3 (ADD_OF | SUB_OF) expression_4
+	| expression_4;
+expression_4:
+	expression_4 (MUL_OF | DIV_OF | MOD_OF) expression_5
+	| expression_5;
+expression_5: NOT_OP expression_5 | expression_6;
+expression_6: SUB_OF expression_6 | expression_7;
+expression_7:
+	array_value
+	| index_expr
 	| func_call_stat
+	| LB expression RB
 	| NUMBERLIT
 	| TRUE
 	| FALSE
--- src/test/ParserSuite_VoTien.py
+++ src/test/ParserSuite_VoTien.py
@@ -165,3 +165,110 @@ class ParserSuite_VoTien(unittest.TestCase):
         input = """func main(number a) """
         expect = "Error on line 1 col 20: <EOF>"
         self.assertTrue(TestParser.test(input, expect, 417))
+        
+    def test_418(self):
+        input = """ var VoTien <- "Vo" ... "Tien" 
+"""
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 418))
+        
+    def test_419(self):
+        input = """ var VoTien <- "Vo" ... 1 ... "Tien" 
+        """
+        expect = "Error on line 1 col 26: ..."
+        self.assertTrue(TestParser.test(input, expect, 419))
+        
+    def test_420(self):
+        input = """ 
+            var VoTien <- true > "true" 
+            var VoTien <- true >= "true"
+            var VoTien <- true = "true"
+            var VoTien <- true == "true"
+            var VoTien <- true < "true"
+            var VoTien <- true <= "true"
+            var VoTien <- true >= "true" ... 1 > 2
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 420))
+        
+    def test_421(self):
+        input = """ var VoTien <- true > x >= z 
+        """
+        expect = "Error on line 1 col 24: >="
+        self.assertTrue(TestParser.test(input, expect, 421))
+        
+    def test_422(self):
+        input = """ 
+            var VoTien <- true and "true" or 1 
+            var VoTien <- 1 and 2 and 3 or 4 or 4
+            var VoTien <- 1 + 2 - 2 + 3 and 3
+            var VoTien <- 1 / 2 * 3 % 4
+            var VoTien <- 1 / 2 / 2 * 3 % 4
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 422))
+        
+    def test_423(self):
+        input = """var VoTien <- true >= "true" and 1 > 2
+        """
+        expect = "Error on line 1 col 35: >"
+        self.assertTrue(TestParser.test(input, expect, 423))
+        
+    def test_424(self):
+        input = """ 
+            var VoTien <- -1 * not 1
+            var VoTien <- not not not----C
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 424))
+        
+    def test_425(self):
+        input = """var VoTien <- - not 1
+        """
+        expect = "Error on line 1 col 16: not"
+        self.assertTrue(TestParser.test(input, expect, 425))
+        
+    def test_426(self):
+        input = """ 
+            var VoTien <- a[1] + 1
+            var VoTien <- array[1,1+2][1][2,3]
+            var VoTien <- array[1,(1)...2,array[ar[(1*2) and 1]],array[2]]
+            var VoTien <- a[1] + fun()[1,fun()] 
+            var VoTien <- 1[1]
+        """
+        expect = "Error on line 3 col 38: ["
+        self.assertTrue(TestParser.test(input, expect, 426))
+        
+    def test_427(self):
+        input = """var VoTien <- a[]
+        """
+        expect = "Error on line 1 col 16: ]"
+        self.assertTrue(TestParser.test(input, expect, 427))
+        
+    def test_428(self):
+        input = """ 
+            var VoTien <- a()
+            var VoTien <- a(1,2)
+            var VoTien <- a(x,array[2])[2]
+            var VoTien <- a(z,k[3] ... 2)[1,2]
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 428))
+        
+    def test_429(self):
+        input = """var VoTien <- a()()
+        """
+        expect = "Error on line 1 col 17: ("
+        self.assertTrue(TestParser.test(input, expect, 429))
+        
+    def test_430(self):
+        input = """ 
+            var VoTien <- a() + ++1 / 2 *3 <= 3 ... "v" >= 2
+            var VoTien <- a(1,2)[1,2,3 ... 2] + false + true
+            var VoTien <- a(z,k[2,3,"2"] ... 2)[true]
+            var VoTien <- (a ... 3) ... b and (a >= b) < b[1, b[1]]
+            var VoTien <-  ["tr", 2, 3, 4, 5] + [[1, 2 + 2 * 2 / 3, 3], [4, 5, 6]]
+            var VoTien <- a(x,array[2])[2,3+2,true,false]
+        """
+        expect = "successful"
+        self.assertTrue(TestParser.test(input, expect, 430))
